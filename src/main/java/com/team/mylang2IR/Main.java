d9f44bcbe7709c8a4b4105b4d1d94a8bf2599058
ilgazer <er.ilgaz@gmail.com>
2021-04-18 19:38:24 +0300

    Fixed Java 11 dependency

--- src/main/java/com/team/mylang2IR/Main.java
+++ src/main/java/com/team/mylang2IR/Main.java
@@ -1,8 +1,8 @@
 package com.team.mylang2IR;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) throws IOException {
         //TODO bir bug vardi ama ne oldugunu unuttum
-        List<String> lines = Files.readAllLines(Path.of("test.txt"));
+        List<String> lines = Files.readAllLines(new File("test.txt").toPath());
         String rawStr = lines.stream().map(s -> s.replaceAll("#.*", "")).collect(Collectors.joining("\n"));
         Scanner s = new Scanner(rawStr);
         try {
