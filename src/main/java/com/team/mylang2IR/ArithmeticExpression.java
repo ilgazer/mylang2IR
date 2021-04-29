3f457b2c458525fb6caa56afb9f1ca4eb36e1459
ilgazer <er.ilgaz@gmail.com>
2021-04-30 01:47:35 +0300

    Added javadocs for ArithmeticExpression

--- src/main/java/com/team/mylang2IR/ArithmeticExpression.java
+++ src/main/java/com/team/mylang2IR/ArithmeticExpression.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ArithmeticExpression extends Expression {
+    /**
+     * Defines the four arithmetic operations defined in mylang along with the corresponding opcodes and priorities
+     */
     public enum Operation {
         ADD('+', "add", 1), SUBTRACT('-', "sub", 1),
         MULTIPLY('*', "mul", 2), DIVIDE('/', "sdiv", 2),
@@ -27,15 +30,40 @@ public class ArithmeticExpression extends Expression {
             this.precedence = precedence;
         }
 
+        /**
+         * Compares the precedence of two {@code Operation} objects.
+         *
+         * @param other the {@code Operation} to be compared.
+         * @return the value {@code 0} if this {@code Operation} has the same precedence;
+         * a value less than {@code 0} if this {@code Operation} has lower precedence than the argument {@code Operation};
+         * a value greater than {@code 0} if this {@code Operation} has higher precedence than the argument {@code Operation}
+         */
         public int comparePrecedence(Operation other) {
             return Integer.compare(precedence, other.precedence);
         }
 
+        /**
+         * Compares the precedence of two {@code Operation} objects.
+         * The value returned is identical to what would be returned by:
+         * <pre>
+         *    Integer.valueOf(x).compareTo(Integer.valueOf(y))
+         * </pre>
+         *
+         * @param sign1 the first {@code Operation} to compare
+         * @param sign2 the second {@code Operation} to compare
+         * @return the value {@code 0} if {@code sign1} and {@code sign2} have the same precedence;
+         * a value less than {@code 0} if {@code sign1} has lower precedence than the {@code sign2};
+         * a value greater than {@code 0} if {@code sign1} has higher precedence than the {@code sign2}
+         */
         public static int comparePrecedence(char sign1, char sign2) {
             return ArithmeticExpression.Operation.getOperationFrom(sign1)
                     .comparePrecedence(ArithmeticExpression.Operation.getOperationFrom(sign2));
         }
 
+        /**
+         * @param sign the sign corresponding to the arithmetic operation
+         * @return the {@code Operation} corresponding to the {@code sign}, or {@code Operation.DEFAULT} if none match
+         */
         public static Operation getOperationFrom(char sign) {
             return CHARACTER_OPERATION_MAP.getOrDefault(sign, DEFAULT);
         }
@@ -45,8 +73,13 @@ public class ArithmeticExpression extends Expression {
     private final Expression leftTerm;
     private final Expression rightTerm;
 
-    public ArithmeticExpression(char operation, Expression leftTerm, Expression rightTerm) {
-        this.operation = Operation.getOperationFrom(operation);
+    /**
+     * @param operator the operator character
+     * @param leftTerm the expression corresponding to the left term
+     * @param rightTerm the expression corresponding to the right term
+     */
+    public ArithmeticExpression(char operator, Expression leftTerm, Expression rightTerm) {
+        this.operation = Operation.getOperationFrom(operator);
         this.leftTerm = leftTerm;
         this.rightTerm = rightTerm;
     }
